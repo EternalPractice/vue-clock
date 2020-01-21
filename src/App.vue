bb857f731e57a59800f576c307dc5dc0b7f3a7b0
Alex Irvine <alex.b.irvine@gmail.com>
2020-01-21 17:47:33 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Clock />
+  </div>
+</template>
+
+<script>
+import Clock from './components/Clock.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Clock
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  background-color: black;
+  height: 100vh;
+  width: 100vw;
+  display: grid;
+  place-items: center;
+}
+</style>
---
+++ src/components/Clock.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="Clock">{{ time }}</div>
+</template>
+
+<script>
+export default {
+  name: 'Clock',
+  data: function() {
+    return { time: 'time' };
+  },
+  mounted: function showTime() {
+    window.setInterval(() => {
+      var date = new Date();
+      var h = date.getHours();
+      var m = date.getMinutes();
+      var s = date.getSeconds();
+      var session = 'AM';
+
+      if (h == 0) {
+        h = 12;
+      }
+
+      if (h > 12) {
+        h = h - 12;
+        session = 'PM';
+      }
+
+      h = h < 10 ? '0' + h : h;
+      m = m < 10 ? '0' + m : m;
+      s = s < 10 ? '0' + s : s;
+
+      this.time = h + ':' + m + ':' + s + ' ' + session;
+    }, 1000);
+  }
+};
+</script>
+
+<style scoped>
+#Clock {
+  color: #17d4fe;
+  font-size: 60px;
+  font-family: Orbitron;
+  letter-spacing: 7px;
+}
+</style>
